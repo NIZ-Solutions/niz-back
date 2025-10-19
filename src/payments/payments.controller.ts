e66a8ed771937ac9c409094a474d82ab0d659e23
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-10-19 22:08:01 +0900

    Fix : controller error handing
    1. 현재 컨트롤러에서 catch문이 서버에서 내려준 에러를 다 500으로 내뱉는 것 같아 수정해보았습니다.
    2. 수정안이 괜찮다면 결제 취소건 (아래) 에도 동일하게 적용해주세요

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -56,12 +56,17 @@ export class PaymentsController {
     if (!dto?.paymentId)
       throw new BadRequestException('결제 정보가 불완전합니다.');
 
-    try {
-      return await this.paymentsService.completePayment(dto, userId);
-    } catch (error) {
-      this.logger.error('결제 완료 처리 중 오류 발생', error);
-      throw new InternalServerErrorException('결제 처리 중 서버 오류가 발생했습니다.');
-    }
+    // try {
+    //   return await this.paymentsService.completePayment(dto, userId);
+    // } catch (error) {
+    //   this.logger.error('결제 완료 처리 중 오류 발생', error);
+    //   throw new InternalServerErrorException('결제 처리 중 서버 오류가 발생했습니다.');
+    // }
+
+    // 위에있는 catch가 오류를 단순한 500으로 내뱉게끔 하고 있어서 
+    // 이렇게 수정해주시면 감사하겠습니다.
+    // 결제 취소도 추후에 테스트후에 그냥 return문으로 수정해주세요.
+    return this.paymentsService.completePayment(dto, userId);
   }
 
   // 결제 취소
@@ -77,6 +82,8 @@ export class PaymentsController {
     if (!dto.paymentId)
       throw new BadRequestException('paymentId가 필요합니다.');
 
+    // ex.
+    // return this.paymentsService.cancelPayment(dto.paymentId);
     try {
       return await this.paymentsService.cancelPayment(dto.paymentId);
     } catch (error) {

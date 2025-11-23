469437f4bde8b7462336fe2acf64189259339136
psy <psy915@swu.ac.kr>
2025-11-23 11:45:35 +0900

    feat: payments.module에 AdminPaymentsController 추가

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,11 +1,11 @@
-// payments.module.ts
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { PrismaService } from '../prisma.service';
+import { AdminPaymentsController } from '../admin/admin-payments.controller';
 
 @Module({
-  controllers: [PaymentsController],
+  controllers: [PaymentsController, AdminPaymentsController],
   providers: [PaymentsService, PrismaService],
 })
 export class PaymentsModule {}
